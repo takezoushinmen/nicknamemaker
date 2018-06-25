2cc3b65ea08aec27a4c5654ebe03f291e12ff46c
Takezou.Shinmen <ec2-user@ip-172-31-17-29.us-east-2.compute.internal>
2018-06-25 01:16:45 +0000

    home botton added

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
+    <ul>
+        <a href="{{action('NicknameController@welcome')}}">
+            <li class="fa fa-home" style="font-size:35px;color:white"></li>
+        </a>
+    </ul>
 @foreach($items as $item)
     <li><div class="history">{{$item->nickname}}<div></li>
 @endforeach
